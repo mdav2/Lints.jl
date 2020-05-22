00af63a8f28957b3e03f59986fc17c163a3c446a
MattDavis <matthew.davis010@gmail.com>
2020-05-22 16:14:32 -0400

    remove libint src

--- libint/libint-2.6.0/tests/unit/c-api-util.cc
+++
@@ -1,6 +0,0 @@
-#include <libint2/boys.h>
-
-/* computes F[0] .. F[max_m] */
-extern "C" void calc_f(double* F, double T, unsigned int max_m) {
-  libint2::FmEval_Chebyshev7<double>::instance(max_m)->eval(F, T, max_m);
-}
